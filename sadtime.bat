502483be2dbadd0967533a246d04b58de220844b
xXRoboBoyXx <redstonemachinery@gmail.com>
2018-12-06 14:07:28 -0500

    i change and do fix

--- sadtime.bat
+++ sadtime.bat
@@ -1,4 +1,4 @@
-git pull
+git pull https://redstonemachinery@gmail.com:Gamer2005@github.com/itsyourboychipsahoy/website.git master
 git add .
 git commit -a
-git push -u origin master
+git push https://redstonemachinery:Gamer2005@github.com/itsyourboychipsahoy/website.git master
