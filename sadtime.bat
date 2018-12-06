57ad803cb2224ce3650d840bff53acdcf7136520
xXRoboBoyXx <redstonemachinery@gmail.com>
2018-12-06 14:09:04 -0500

    <3

--- sadtime.bat
+++ sadtime.bat
@@ -1,4 +1,4 @@
 git pull https://redstonemachinery@gmail.com:Gamer2005@github.com/itsyourboychipsahoy/website.git master
 git add .
 git commit -a
-git push https://redstonemachinery:Gamer2005@github.com/itsyourboychipsahoy/website.git master
+git push https://redstonemachinery@github.com:Gamer2005/itsyourboychipsahoy/website.git master
